aecbd2a6999c38b3fa1edab6116366151f7dfe5c
abhishekh-westack <abhishekh.ojha@westack.ai>
2025-09-18 17:18:37 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { Editor } from './Editor'
+
+function App() {
+
+  return (
+    <>
+      <Editor />
+    </>
+  )
+}
+
+export default App
---
+++ src/Editor.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { useEditor, EditorContent } from "@tiptap/react";
+import StarterKit from "@tiptap/starter-kit";
+
+export const Editor: React.FC = () => {
+  const editor = useEditor({
+    extensions: [StarterKit],
+    content: "<p>Hello World! ✍️</p>",
+  });
+
+  return <EditorContent editor={editor} className="prose kali-editorcontent" />;
+};
